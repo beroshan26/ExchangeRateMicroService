9b18b26eab8e0dfca596aa8b8cc3c2f58c1f690f
beroshan26 <32231715+beroshan26@users.noreply.github.com>
2019-05-24 16:27:08 +1000

    added moq

--- ExchangeRateAPITests/ApiMockTests.cs
+++ ExchangeRateAPITests/ApiMockTests.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Moq;
+using ExchangeRateWebApi.API;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExchangeRateWebApi.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using ExchangeRateWebApi.Model;
+using ExchangeRateWebApi.Helpers;
+using ExchangeRateWebApi.Cache;
+using ExchangeRateWebApi.Validators;
 
 namespace ExchangeRateAPITests
 {
+    [TestClass]
     public class ApiMockTests
     {
+        [TestMethod]
+        public void FixerApiMockTest()
+        {
+            var apimock = new Mock<IFixerApi>();           
+            var configMock = new Mock<IConfiguration>();
+
+            FixerIOController ctrl = new FixerIOController(configMock.Object, apimock.Object);
+            var result = ctrl.Get("EUR", "USD");
+            Assert.AreEqual(result.GetType(), typeof(JsonResult));
+        }
     }
 }
